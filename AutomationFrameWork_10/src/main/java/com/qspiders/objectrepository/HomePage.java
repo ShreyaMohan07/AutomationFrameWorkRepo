49eb5f80a5be292d7bd186cfe816988dc5c13d50
HP <HP@DESKTOP-TPUJN1N>
2025-02-28 17:18:17 +0530

    adding framework

---
+++ AutomationFrameWork_10/src/main/java/com/qspiders/objectrepository/HomePage.java
@@ -0,0 +1,32 @@
+package com.qspiders.objectrepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+	@FindBy(linkText="Log out")
+	 private WebElement logoutLink;
+	@FindBy(partialLinkText="BOOKS")
+	 private WebElement booksLink;
+	@FindBy(partialLinkText="COMPUTERS")
+	 private WebElement computersLink;
+	
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		PageFactory.initElements(driver,this);
+	}
+
+	public WebElement getLogoutLink() {
+		return logoutLink;
+	}
+
+	public WebElement getBooksLink() {
+		return booksLink;
+	}
+
+	public WebElement getComputersLink() {
+		return computersLink;
+}
+}
---
+++ AutomationFrameWork_10/src/test/java/com/qspiders/book/TC_DWS_001_Test.java
@@ -0,0 +1,10 @@
+package com.qspiders.book;
+
+import org.testng.annotations.Test;
+
+public class TC_DWS_001_Test {
+@Test
+public void launch() {
+	System.out.println("executing launch");
+}
+}
