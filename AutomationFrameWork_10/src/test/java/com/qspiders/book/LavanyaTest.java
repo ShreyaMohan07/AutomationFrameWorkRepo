1b23a8a56eca90023309a4e24e4c72d02a902255
pratap kurni <pratap kurni@LAPTOP-AOULPL3Q>
2025-02-28 18:20:32 +0530

    added new sop

--- AutomationFrameWork_10/src/test/java/com/qspiders/book/LavanyaTest.java
+++ AutomationFrameWork_10/src/test/java/com/qspiders/book/LavanyaTest.java
@@ -3,6 +3,7 @@ package com.qspiders.book;
 public class LavanyaTest {
 	public static void main(String[] args) {
 		System.out.println("hello Shreya");
+		System.out.println("added 1 more sop");
 	}
 
 }
